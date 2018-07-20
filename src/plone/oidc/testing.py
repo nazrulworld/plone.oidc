e020874fcf3659696bcbf64f3f1f148026404930
Md Nazrul Islam <connect2nazrul@gmail.com>
2018-07-20 09:16:12 +0200

    Project initialized [nazrulworld]

---
+++ src/plone/oidc/testing.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+
+import plone.oidc
+
+
+class PloneOidcLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load any other ZCML that is required for your tests.
+        # The z3c.autoinclude feature is disabled in the Plone fixture base
+        # layer.
+        self.loadZCML(package=plone.oidc)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'plone.oidc:default')
+
+
+PLONE_OIDC_FIXTURE = PloneOidcLayer()
+
+
+PLONE_OIDC_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONE_OIDC_FIXTURE,),
+    name='PloneOidcLayer:IntegrationTesting',
+)
+
+
+PLONE_OIDC_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(PLONE_OIDC_FIXTURE,),
+    name='PloneOidcLayer:FunctionalTesting',
+)
+
+
+PLONE_OIDC_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        PLONE_OIDC_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE,
+    ),
+    name='PloneOidcLayer:AcceptanceTesting',
+)
